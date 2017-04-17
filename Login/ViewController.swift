4605a3babba3d39509293757c01fbbf4a5b75438
Tung <tung@techmasters-iMac-7.local>
2017-04-17 17:45:11 +0700

    Add Delete

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,16 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfUser: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    var users = ["tung": "1234",
+                 "kien": "567",
+                 "liem": "789"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func actionDangNhap(_ sender: UIButton) {
+        if let password = users[tfUser.text!] {
+            if password == tfPassword.text {
+                print("Đăng nhập thành công.")
+            } else {
+                print("Mật khẩu không đúng.")
+            }
+        } else {
+            print("Tài khoản không tồn tại.")
+        }
     }
-
-
+    
+    @IBAction func actionDanhSach(_ sender: UIButton) {
+        for user in users {
+            print("Tên đăng nhập: \(user.0), mật khẩu: \(user.1)")
+        }
+    }
+    
+    @IBAction func actionThemUser(_ sender: UIButton) {
+        let newUser = tfUser.text
+        let newPass = tfPassword.text
+        var existUser = false
+        
+        if (newUser!.isEmpty)||(newPass!.isEmpty) {
+            print("Không đủ thông tin để tạo tài khoản")
+            existUser = true
+        } else if (newUser![newUser!.startIndex] == " ") {
+            print("Tên người dùng không được bắt đầu bằng khoảng trắng")
+            existUser = true
+        } else {
+            for name in users.keys {
+                if name == newUser {
+                    print("Tài khoản đăng nhập đã tồn tại")
+                    existUser = true
+                }
+            }
+        }
+        
+        if !existUser {
+            print("Đã thêm tài khoản mới")
+            users[newUser!] = newPass!
+        }
+    }
+    
+    @IBAction func actionXoaUser(_ sender: UIButton) {
+        let delUser = tfUser.text
+        let delPass = tfPassword.text
+        var existUser = false
+        
+        if (delUser!.isEmpty)||(delPass!.isEmpty) {
+            print("Không đủ thông tin để xóa tài khoản")
+            existUser = false
+        } else {
+            for name in users {
+                if (name.key == delUser)&&(name.value == delPass) {
+                    existUser = true
+                }
+            }
+        }
+        
+        if existUser {
+            print("Đã xóa tài khoản")
+            users.removeValue(forKey: delUser!)
+        }
+    }
 }
 
